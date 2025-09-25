ab5077f6036518ddbc26a0db27b7f611d1b7156b
nicolaphee <nicola.phee@gmail.com>
2025-09-25 21:52:09 +0200

    remove obsolete files

--- utils/params_classifiers.py
+++
@@ -1,9 +0,0 @@
-random_state = 42
-
-# n_iter = 20  # numero di iterazioni per RandomizedSearchCV
-# apply_feature_eng = True
-# log_transform_target = False  # se True, applica log-transform al target
-# sample_weighting = False  # se True, usa pesi campione
-# use_simple_imputer = True
-
-
